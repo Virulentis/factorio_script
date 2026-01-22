f35a660529df93750ec8a17d5f0d915d4d03f2e9
William <me@wramsay.com>
2026-01-22 15:33:01 -0800

    Updated bash script, starting on adding custom server settings.

--- build_factorio.sh
+++ build_factorio.sh
@@ -7,11 +7,11 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-if [$built -eq 0]; then
+# if [$built -eq 0]; then
     apt update && apt upgrade -y
     echo -e "${BLUE}Creating user and group factorio${NC}"
     adduser --disabled-login -no-create-home --gecos factorio factorio
-    echo -e "${BLUE}created user${NC}"
+    echo -e "${BLUE}Attempted to create user ${NC}"
     cd /opt
     wget -O factorio_headless.tar.gz https://www.factorio.com/get-download/latest/headless/linux64
     tar -xvf factorio_headless.tar.gz
@@ -35,4 +35,9 @@ if [$built -eq 0]; then
 
     systemctl enable factorio
     systemctl start factorio
-fi
+
+    # TODO add custom server settings default
+    echo """
+    ADD custom server-settings.json
+    """ > /opt/factorio/data/server-settings.json
+# fi
