c58dc3fa18c1931ec6512b77e0b5eaec7aad6384
William <me@wramsay.com>
2026-01-22 13:07:18 -0800

    updated shell script

--- build_factorio.sh
+++ build_factorio.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
+echo "creating user"
+adduser --disabled-login -no-create-home --gecos factorio factorio
+echo "created user"
+cd /opt
+wget -O factorio_headless.tar.gz https://www.factorio.com/get-download/latest/headless/linux64
+tar -xvf factorio_headless.tar.gz
+rm factorio_headless.tar.gz
+chown -R factorio:factorio /opt/factorio
