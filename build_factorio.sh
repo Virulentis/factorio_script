1a185fc489f23c2596b1b6d8d327c0e3378cbce4
William <me@wramsay.com>
2026-01-22 15:18:33 -0800

    Updated bash script

--- build_factorio.sh
+++ build_factorio.sh
@@ -7,32 +7,32 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-if [$built -lt 1] then
-apt update && apt upgrade -y
-echo -e "${BLUE}Creating user and group factorio"
-adduser --disabled-login -no-create-home --gecos factorio factorio
-echo -e "$created user"
-cd /opt
-# wget -O factorio_headless.tar.gz https://www.factorio.com/get-download/latest/headless/linux64
-# tar -xvf factorio_headless.tar.gz
-# rm factorio_headless.tar.gz
-echo -e "_________ \ngiving permissions to factorio user"
-chown -R factorio:factorio /opt/factorio
-echo """
-    [Unit]
-    Description=Factorio Headless Server
-    After=network.target
+if [$built -eq 0]; then
+    apt update && apt upgrade -y
+    echo -e "${BLUE}Creating user and group factorio${NC}"
+    adduser --disabled-login -no-create-home --gecos factorio factorio
+    echo -e "${BLUE}created user${NC}"
+    cd /opt
+    wget -O factorio_headless.tar.gz https://www.factorio.com/get-download/latest/headless/linux64
+    tar -xvf factorio_headless.tar.gz
+    rm factorio_headless.tar.gz
+    echo -e "${BLUE}_________ \ngiving permissions to factorio user${NC}"
+    chown -R factorio:factorio /opt/factorio
+    echo """
+        [Unit]
+        Description=Factorio Headless Server
+        After=network.target
 
-    [Service]
-    User=factorio
-    WorkingDirectory=/opt/factorio
-    ExecStart=/opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/YourSaveFile.zip --server-settings /opt/factorio/data/server-settings.json
-    Restart=on-failure
+        [Service]
+        User=factorio
+        WorkingDirectory=/opt/factorio
+        ExecStart=/opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/YourSaveFile.zip --server-settings /opt/factorio/data/server-settings.json
+        Restart=on-failure
 
-    [Install]
-    WantedBy=multi-user.target
- """ > /etc/systemd/system/factorio.service
+        [Install]
+        WantedBy=multi-user.target
+    """ > /etc/systemd/system/factorio.service
 
-systemctl enable factorio
-systemctl start factorio
+    systemctl enable factorio
+    systemctl start factorio
 fi
